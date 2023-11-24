a49948c6eb508a59de367451e6deba6f16eb52d5
GZhonghui <mail@gzher.com>
2023-11-24 17:33:23 +0900

    move final project

--- ParagonRoyalServer/PRGameServer.sh
+++
@@ -1,4 +0,0 @@
-g++ PRGameServer.cpp  GameMessage.pb.cc -o PRGameServer -std=c++11 -lprotobuf -I/usr/include/mysql/ -L/usr/lib64/mysql -L/usr/local/lib/ -lmysqlclient_r
-g++ PRGateServer.cpp  GameMessage.pb.cc -o PRGateServer -std=c++11 -lprotobuf -I/usr/include/mysql/ -L/usr/lib64/mysql -L/usr/local/lib/ -lmysqlclient_r
-g++ PRLoginServer.cpp  GameMessage.pb.cc -o PRLoginServer -std=c++11 -lprotobuf -I/usr/include/mysql/ -L/usr/lib64/mysql -L/usr/local/lib/ -lmysqlclient_r
- 
--- ParagonRoyalServer/pthreadTool.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>   //调用 sleep() 函数
-#include <pthread.h>
-#include <iostream>
-using namespace std;
-class ServerThreadManager{
-
-};
